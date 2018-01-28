37156017a84e2c96e14ac7bc7594175ec533ac5d
ThinMintz <rachel.rodriguez05@gmail.com>
2018-01-27 20:00:12 -0600

    working on handlebars

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 // Database configuration with mongoose
-mongoose.connect("mongodb://localhost/week18day3mongoose");
+mongoose.connect("mongodb://localhost/nasaScrape");
 var db = mongoose.connection;
 
 // Show any mongoose errors
